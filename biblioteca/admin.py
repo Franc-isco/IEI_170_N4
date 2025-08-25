89b91f0602b26874e77f40ca457d932afc030dc1
jfran <francisco.tabilo05@inacapmail.cl>
2025-08-25 16:56:58 -0400

    avance tres

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Biblioteca, Nacionalidad, Lector, Autor, Genero, Libro, Prestamo 
+from .models import Comuna, Biblioteca, Nacionalidad, Lector, Autor, Categoria, Libro, Prestamo, Direccion 
 
 # Register your models here.
+admin.site.register(Comuna)
 admin.site.register(Biblioteca)
 admin.site.register(Nacionalidad)
 admin.site.register(Lector)
 admin.site.register(Autor)
-admin.site.register(Genero)
+admin.site.register(Categoria)
 admin.site.register(Libro)
 admin.site.register(Prestamo)
+admin.site.register(Direccion)
